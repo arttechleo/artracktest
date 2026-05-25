81d698d7f2bca99d37115bfd85e930899d0d4d12
unknown <ledanenkov@gmail.com>
2026-05-25 16:54:10 -0700

    fix: force bundle three, pin to 0.150.0

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',
+  optimizeDeps: {
+    include: ['three']
+  },
   build: {
     target: 'esnext',
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: { main: './index.html' }
+      input: { main: './index.html' },
+      external: []
     }
   }
 })
